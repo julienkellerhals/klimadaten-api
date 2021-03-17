5f47fb4976217ac87ec7c80eddc66ed98ab7fbc6
Julien Kellerhals <julien.kellerhals@gmail.com>
2021-03-17 11:57:13 +0100

    Added automatic driver download

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import io
+import zipfile
+from pathlib import Path
 import sqlalchemy
 from sqlalchemy import create_engine
 from flask import Flask
-
+from flask import request
 import download
 
 app = Flask(__name__)
@@ -14,6 +17,45 @@ def mainPage():
 @app.route("/api")
 def api():
     return "API"
+
+@app.route("/admin/driver/<browser>", methods=['GET'])
+def driver(browser):
+    output = ""
+    cwd = Path.cwd()
+    driverfolder = cwd / "driver"
+    headlessBool = request.args['headless']
+
+    # user agent
+    userAgent = request.headers.get('User-Agent')
+    output += "User agent: " + userAgent + "</br>"
+    for browserVersion in userAgent.split(" "):
+        if browserVersion.split("/")[0] == browser:
+            version = browserVersion.split("/")[1]
+    if len(version) == 0:
+        # output += "Browser not found, options are - Mozilla, AppleWebKit, Chrome, Safari, Edg" + "</br>"
+        output += "Browser not found, options are - Chrome, Edg" + "</br>"
+
+    driverInstalledBool = False
+    for driverPath in list(driverfolder.glob('**/*.exe')):
+        if browser.lower() in driverPath.name:
+            driverInstalledBool = True
+
+    if not driverInstalledBool:
+        # download driver
+        output += "Installing driver" + "</br>"
+        browserDriverConf = {
+            "Edg": "https://msedgedriver.azureedge.net/" + version + "/edgedriver_win64.zip",
+            "Chrome": "https://chromedriver.storage.googleapis.com/" + version + "/chromedriver_win32.zip"
+        }
+        output += "Driver URL: " + browserDriverConf[browser] + "</br>"
+        driverRequest = download.getRequest(browserDriverConf[browser])[0]
+        driverZip = zipfile.ZipFile(io.BytesIO(driverRequest.content))
+        driverZip.extractall(driverfolder)
+        output += "Downloaded and extracted driver" + "</br>"
+    else:
+        output += "Driver already satisfied" + "</br>"
+
+    return output
 
 @app.route("/admin/refresh")
 def refreshData():
