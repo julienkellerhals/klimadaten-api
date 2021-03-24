1e7fcfe3e6e48374cc4dd91bc45c375e7e1fdc0b
lars.altschul <lars.al@hotmail.com>
2021-03-24 22:21:57 +0100

    Added classes for pytest

--- webscraping_test.py
+++ webscraping_test.py
@@ -32,6 +32,21 @@ else:
 driver = abstractDriver.createDriver(browser, driverPath, False)
 
 
-def test_meteoschweiz_title():
-    driver.get('https://www.meteoschweiz.admin.ch/home/klima/schweizer-klima-im-detail/homogene-messreihen-ab-1864.html?region=Tabelle')
-    assert driver.title == 'Homogene Messreihen ab 1864 - MeteoSchweiz'
+@pytest.mark.meteoschweiz
+class TestMeteoSchweiz():
+    def test_meteoschweiz_title(self):
+        driver.get('https://www.meteoschweiz.admin.ch/home/klima/schweizer-klima-im-detail/homogene-messreihen-ab-1864.html?region=Tabelle')
+        assert driver.title == 'Homogene Messreihen ab 1864 - MeteoSchweiz'
+
+    def test_meteoschweiz_xpath(self):
+        urls = driver.find_elements_by_xpath(
+            "//table[@id='stations-table']/tbody/tr"
+            + "/td/span[@class='overflow']/a")
+        assert len(urls) == 30
+
+
+@pytest.mark.idaweb
+class TestIDAWeb():
+    def test_idaweb_title(self):
+        driver.get("https://gate.meteoswiss.ch/idaweb/login.do")
+        assert driver.title == 'MeteoSchweiz IDAWEB: Anmelden bei IDAWEB'
