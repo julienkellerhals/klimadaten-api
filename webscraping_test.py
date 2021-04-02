b7a5ea3eb4fc930773126794dc187cb95964b113
Julien Kellerhals <julien.kellerhals@gmail.com>
2021-04-02 15:10:29 +0200

    Changed from driver close to quit

--- webscraping_test.py
+++ webscraping_test.py
@@ -6,24 +6,32 @@ from selenium.webdriver.chrome.options import Options
 import abstractDriver
 
 abstractDriver = abstractDriver.AbstractDriver()
-driver = abstractDriver.getDriver()
 
 
 @pytest.mark.meteoschweiz
 class TestMeteoSchweiz():
     def test_meteoschweiz_title(self):
+        driver = abstractDriver.getDriver()
         driver.get('https://www.meteoschweiz.admin.ch/home/klima/schweizer-klima-im-detail/homogene-messreihen-ab-1864.html?region=Tabelle')
-        assert driver.title == 'Homogene Messreihen ab 1864 - MeteoSchweiz'
+        title = driver.title
+        driver.quit()
+        assert title == 'Homogene Messreihen ab 1864 - MeteoSchweiz'
 
     def test_meteoschweiz_xpath(self):
+        driver = abstractDriver.getDriver()
+        driver.get('https://www.meteoschweiz.admin.ch/home/klima/schweizer-klima-im-detail/homogene-messreihen-ab-1864.html?region=Tabelle')
         urls = driver.find_elements_by_xpath(
             "//table[@id='stations-table']/tbody/tr"
             + "/td/span[@class='overflow']/a")
+        driver.quit()
         assert len(urls) == 30
 
 
 @pytest.mark.idaweb
 class TestIDAWeb():
     def test_idaweb_title(self):
+        driver = abstractDriver.getDriver()
         driver.get("https://gate.meteoswiss.ch/idaweb/login.do")
-        assert driver.title == 'MeteoSchweiz IDAWEB: Anmelden bei IDAWEB'
+        title = driver.title
+        driver.quit()
+        assert title == 'MeteoSchweiz IDAWEB: Anmelden bei IDAWEB'
