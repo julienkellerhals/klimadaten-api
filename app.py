f7e43c1985932caed8ebb9a7e55246415c0f8734
Julien Kellerhals <julien.kellerhals@gmail.com>
2021-04-28 17:15:10 +0200

    Added favicon to remove 404 error

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from flask import send_from_directory
 import db
 import abstractDriver
 import messageAnnouncer
@@ -63,3 +65,12 @@ def api():
     """
 
     return "API"
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
