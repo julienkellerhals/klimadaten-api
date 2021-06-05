b168e0dc8a2c8ceb07e3ba0d1966eaab28f28c14
GrosjeanJoel <joel.twann@bluewin.ch>
2021-06-05 16:40:33 +0200

    Added test for refresh date table columns

--- db_test.py
+++ db_test.py
@@ -39,9 +39,14 @@ class TestDatabase():
 
     # Test if refresh dates are in a dataframe
     # and dataframe has at least one entry
-    def testGetParameterRefreshDate(self):
+    def testGetParameterRefreshDateSuccess(self):
         table = instance.getParameterRefreshDate()
         if type(table) == pd.core.frame.DataFrame:
             assert len(table) > 0
         else:
             assert False
+
+    # Test if the refresh dates table has the necessary columns
+    def testGetParameterRefreshDateColumns(self):
+        table = instance.getParameterRefreshDate()
+        assert 'meas_name' and 'valid_from' in table.columns
